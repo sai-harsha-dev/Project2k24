a254f5366074cdf66c4d1ee650117579bc13d58b
HARSHA <vns.palanati@gmail.com>
2024-10-26 11:02:35 +0530

    Web first iteration- Modular, coloured output, stdout stderr redirected

---
+++ scripts/Web/nginx_setup.sh
@@ -0,0 +1,41 @@
+#!/bin/bash      
+  
+source ../Common/pg_status
+
+sudo apt update 1>>output.log 2>>error.log &
+status $? 'updated repos' $! 'updating repos'
+
+sudo apt install nginx -y 1>>output.log 2>>error.log &
+status $? 'installed nginx' $! 'installing nginx'
+
+sudo cp default.conf.template /etc/nginx/conf.d/ 1>>output.log 2>>error.log &
+status $? 'copied new config' $! 'copying new config file'
+
+sudo mv frontend.tar /usr/share/nginx/ 1>>output.log 2>>error.log &
+status $? 'moved frontend.tar to nginx source' $! 'moving frontend.tar to nginx source'
+
+cd /usr/share/nginx/; sudo rm html/index.html 1>>output.log 2>>error.log &
+status $? 'removed default index.html' $! 'removing default index.html'
+
+sudo tar xvf frontend.tar -C html/ --strip-components=1  1>>output.log 2>>error.log &
+status $? 'unzipped source folders' $! 'unzipping source folders'
+
+# sudo mv index.html /usr/share/nginx/html 1>>output.log 2>>error.log &
+# status $? 'moved index.html to nginx source' $! 'moving index.html to nginx source'
+
+# sudo mv media -C /usr/share/nginx/html/ 1>>output.log 2>>error.log &
+# status $? 'moving media to nginx source' $! 'extracting media'
+
+# sudo tar xvf images.tar -C /usr/share/nginx/html/ 1>>output.log 2>>error.log &
+# status $? 'extrated images' $! 'extracting images'
+
+sudo cat include /etc/nginx/conf.d/* > /etc/nginx/nginx.conf 1>>output.log 2>>error.log &
+status $? 'updated nginx.conf' $! 'updating nginx.conf'
+
+sudo systemctl start nginx 1>>output.log 2>>error.log &
+status $? 'started nginx' $! 'starting nginx'
+
+sudo systemctl enable nginx 1>>output.log 2>>error.log &
+status $? 'enabled nginx' $! 'enabling nginx'
+
+sudo systemctl status nginx
