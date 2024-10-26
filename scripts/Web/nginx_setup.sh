f68457c8b5663ce3ed547b25f9f6c6788edc2ce7
HARSHA <vns.palanati@gmail.com>
2024-10-26 20:01:10 +0530

    Iteration two Inline File manipuldation, added realpath, dirname, update fronted.tar extart location

--- scripts/Web/nginx_setup.sh
+++ scripts/Web/nginx_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash      
-script_dir=$(dirname $0)
-base_dir=$(dirname $script_dir | xargs realpath)
+real_script_dir=$(dirname $0 | xargs realpath)
+base_dir=$(dirname $real_script_dir | xargs realpath)
 source $base_dir/Common/pg_status.sh
 
 sudo apt update 1>>output.log 2>>error.log &
@@ -9,28 +9,22 @@ status $? 'updated repos' $! 'updating repos'
 sudo apt install nginx -y 1>>output.log 2>>error.log &
 status $? 'installed nginx' $! 'installing nginx'
 
-sudo cp default.conf.template /etc/nginx/conf.d/ 1>>output.log 2>>error.log &
+sudo sed -i 's/${[A-Z]\+_HOST}/localhost/g' ${real_script_dir}/default.conf.template 1>>output.log 2>>error.log &
+status $? 'updated host address to localhost in new config' $! 'updating host address to localhost in copying new config file'
+
+sudo mv  ${real_script_dir}/default.conf.template /etc/nginx/conf.d/ 1>>output.log 2>>error.log &
 status $? 'copied new config' $! 'copying new config file'
 
-sudo mv frontend.tar /usr/share/nginx/ 1>>output.log 2>>error.log &
+sudo mv ${real_script_dir}/frontend.tar /usr/share/nginx/ 1>>output.log 2>>error.log &
 status $? 'moved frontend.tar to nginx source' $! 'moving frontend.tar to nginx source'
 
 cd /usr/share/nginx/; sudo rm html/index.html 1>>output.log 2>>error.log &
 status $? 'removed default index.html' $! 'removing default index.html'
 
-sudo tar xvf frontend.tar -C html/ --strip-components=1  1>>output.log 2>>error.log &
+sudo tar xvf usr/share/nginx/frontend.tar -C /usr/share/nginx/html/ 1>>output.log 2>>error.log &
 status $? 'unzipped source folders' $! 'unzipping source folders'
 
-# sudo mv index.html /usr/share/nginx/html 1>>output.log 2>>error.log &
-# status $? 'moved index.html to nginx source' $! 'moving index.html to nginx source'
-
-# sudo mv media -C /usr/share/nginx/html/ 1>>output.log 2>>error.log &
-# status $? 'moving media to nginx source' $! 'extracting media'
-
-# sudo tar xvf images.tar -C /usr/share/nginx/html/ 1>>output.log 2>>error.log &
-# status $? 'extrated images' $! 'extracting images'
-
-sudo cat include /etc/nginx/conf.d/*; > /etc/nginx/nginx.conf 1>>output.log 2>>error.log &
+sudo sed -i -e '1i include /etc/nginx/conf.d/*;' -e '1,$d' /etc/nginx/nginx.conf 1>>output.log 2>>error.log &
 status $? 'updated nginx.conf' $! 'updating nginx.conf'
 
 sudo systemctl start nginx 1>>output.log 2>>error.log &
