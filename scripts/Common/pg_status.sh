ac6b25f09af0991627058e6482c47fbabc735bca
HARSHA <vns.palanati@gmail.com>
2024-10-26 21:28:21 +0530

    Added \e colour code escape sequence

--- scripts/Common/pg_status.sh
+++ scripts/Common/pg_status.sh
@@ -1,23 +1,23 @@
 #!/bin/bash      
   
-BLUE='\033[0:34m'
-RED='\033[0:31m'
-GREEN='\033[0:32m'
+BLUE='[34m'
+RED='[31m'
+GREEN='[32m'
 
 status(){
     while  kill -0 $3 2> out.log;
     do
-    echo -ne '\E[101M]'"${BLUE}${4}..."
+    echo -ne "\e${BLUE}${4}..."
     echo -ne "\r"
-    sleep 0.5
+    sleep 0.2
     done
 
     if [ $1 -ne 0 ];
         then
-        echo -e "${RED}\rERROR: $(cat <2)"
+        echo -e "\e${RED}\rERROR: $(cat <2)"
         exit 0
     else
-        echo -e "${GREEN}\rSUCCESS: $2"
+        echo -e "\e${GREEN}\rSUCCESS: $2"
         return 0
     fi
 }
